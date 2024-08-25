ea7fbc744c141cbd561f914cd6ec25795faf3739
hinataloli <100828189+hinataloli@users.noreply.github.com>
2024-08-26 01:57:13 +0700

    Update q.php

--- q.php
+++ q.php
@@ -1,6 +1,6 @@
 <?php
 
-    $Url = "https://gist.githubusercontent.com/ManiMan43/11de31117ef25ced828d063c868d9496/raw/1236857759f8d93215ebe251c8716ebc791838b0/1.txt";
+    $Url = "https://raw.githubusercontent.com/hinataloli/haha/main/abo.txt";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
